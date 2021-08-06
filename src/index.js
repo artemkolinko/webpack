976def3d640b3bcd6a84fcf8b29f6494fe722af2
Artem Kolinko <kalinart@gmail.com>
2021-08-06 15:15:09 +0300

    Add asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import './style.css';
+import Icon from './png.png';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack', '5'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' + ');
+  const words = ['Hello', 'webpack'];
+  element.innerHTML = words.join(' ');
+  element.classList.add('hello');
+  console.log(Icon);
+
+  // Add the image to our existing div.
+  const myIcon = new Image(32, 32);
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
