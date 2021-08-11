cb58afbd1409ff8579bf3be11679283669ea2dad
Artem Kolinko <kalinart@gmail.com>
2021-08-11 13:20:52 +0300

    Add mini-css-extract-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // entry: './src/index.js',
@@ -35,12 +36,15 @@ module.exports = {
       template: './pug/pages/contacts.pug',
       filename: './contacts.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.pug$/,
